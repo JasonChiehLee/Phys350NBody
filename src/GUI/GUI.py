d90b8dfa6fa5b9a5928b3f619a659e3dc5e4afdc
Jason Chieh Lee <jasonunivlee@hotmail.com>
2017-03-18 19:33:00 -0700

    Worked on sliders a lil more

--- src/GUI/GUI.py
+++ src/GUI/GUI.py
@@ -1,11 +1,24 @@
-from Tkinter import*
+from tkinter import*
 root = Tk()
-logo = PhotoImage(file='./orbit_logo.gif')
-w1 = Label(root, image=logo).pack(side="right")
-button = Button(root, text='QUIT', fg="red", command=quit)
-button.pack(side=BOTTOM)
+
 desc = "The Odd Squad Presents: 3 Body Orbital Mechanics"
 
-w2 = Label(root,
-    justify=LEFT, padx = 10, text=desc).pack(side="left")
+w1 = Label(root,
+    justify=LEFT, padx = 10, text=desc).pack()
+
+logo = PhotoImage(file='./orbit_logo.gif')
+w2 = Label(root, image=logo).pack()
+
+mass_slider1 = Scale(root, from_=0, to= 1000,label="Mass 1",length=150, orient=HORIZONTAL)
+mass_slider1.pack()
+
+mass_slider2 = Scale(root, from_=0, to= 1000,label="Mass 2",length=150, orient=HORIZONTAL)
+mass_slider2.pack()
+
+mass_slider3 = Scale(root, from_=0, to= 1000,label="Mass 3",length=150, orient=HORIZONTAL)
+mass_slider3.pack()
+
+
+button = Button(root, text='QUIT', fg="red", command=quit)
+button.pack()
 root.mainloop()
