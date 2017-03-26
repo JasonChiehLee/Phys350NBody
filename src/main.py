79d3f6236162595686d943ca08ca33ce7c9f8488
dn793 <dnguyen793@gmail.com>
2017-03-25 17:09:33 -0700

    New main.py with code changes

--- src/main.py
+++ src/main.py
@@ -2,12 +2,13 @@
 
 from object import Object
 from plot import Plot
+import physics as phys
 
-PLOT = Plot(0, 200, 0, 200, 100)
+PLOT = Plot(0, 200, 0, 200)
 
-OBJ_1 = Object(10.0, 20, 100.0, 150.0, 3.0, 4.0)
-OBJ_2 = Object(20.0, 15, 50.0, 75.0, 3.0, 4.0)
-OBJ_3 = Object(5.0, 10, 150.0, 10.0, 3.0, 4.0)
+OBJ_1 = Object(10.0, 20, phys.State(100.0, 150.0, 3.0, 4.0))
+OBJ_2 = Object(20.0, 15, phys.State(50.0, 75.0, 3.0, 4.0))
+OBJ_3 = Object(5.0, 10, phys.State(150.0, 10.0, 3.0, 4.0))
 
 COLOUR_1 = 'red'
 COLOUR_2 = 'green'
@@ -17,4 +18,9 @@ PLOT.place_obj(OBJ_1, COLOUR_1)
 PLOT.place_obj(OBJ_2, COLOUR_2)
 PLOT.place_obj(OBJ_3, COLOUR_3)
 
-PLOT.show()
+print(phys.get_accel(OBJ_1.state))
+print(2 * phys.get_accel(OBJ_1.state))
+
+
+
+
