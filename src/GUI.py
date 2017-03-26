097057f002178078d9e9aa72b29b46df5c893d63
Jason Chieh Lee <jasonunivlee@hotmail.com>
2017-03-25 23:07:02 -0700

    refactored code, improved consistency of methods, added methods to gui classes, added default values as 0 for entries. Struggling with getting sliders to set entries though still working on that

--- src/GUI.py
+++ src/GUI.py
@@ -8,36 +8,36 @@ from plot import *
 root = Tk()
 root.wm_title("PHYS 350: 3 Body Orbital Mechanics")
 
-masses = [0, 0, 0]
-vel = [0, 0, 0]
+mass_list = [0, 0, 0]
+vel_list = [0, 0, 0]
 
 def butt_set_mass():
     for i in range(0,3):
-        masses[i] = int(mass_entries[i].get_entry())
-        mass_sliders[i].set_value(int(mass_entries[i].get_entry()))
-        print(masses[i])
+        mass_list[i] = mass_entries[i].get_value()
+        mass_sliders[i].set_value(mass_list[i])
 
-def butt_set_v():
+def butt_set_vel():
     for i in range(0,3):
-        vel[i] = int(v_entries[i].get_entry())
-        print(vel[i])
+        vel_list[i] = vel_entries[i].get_value()
+        vel_sliders[i].set_value(vel_list[i])
 
-mass_entries = [Text_Entry(root, 2, 0), Text_Entry(root, 2, 1), \
-    Text_Entry(root, 2, 2)]
+mass_entries = [Text_Entry(root, 0, 2, 0), \
+    Text_Entry(root, 0, 2, 1), \
+    Text_Entry(root, 0, 2, 2)]
 
-v_entries = [Text_Entry(root, 6, 0), Text_Entry(root, 6, 1), \
-    Text_Entry(root, 6, 2)]
+vel_entries = [Text_Entry(root, 0, 6, 0), Text_Entry(root, 0, 6, 1), \
+    Text_Entry(root, 0, 6, 2)]
 
 mass_sliders = [Slider(root, 0, 1000, 150, HORIZONTAL, 3, 0), \
      Slider(root, 0, 1000, 150, HORIZONTAL, 3, 1), \
      Slider(root, 0, 1000, 150, HORIZONTAL, 3, 2)]
 
-v_sliders = [Slider(root, 0, 1000, 150, HORIZONTAL, 7, 0), \
+vel_sliders = [Slider(root, 0, 1000, 150, HORIZONTAL, 7, 0), \
     Slider(root, 0, 1000, 150, HORIZONTAL, 7, 1),
-    Slider(root, 0, 1000, 150, HORIZONTAL, 7, 2)]
+    Slider(root,  0, 1000, 150, HORIZONTAL, 7, 2)]
 
 butts = [Push_Button(root, 'Set Mass', butt_set_mass, 4, 1), \
-    Push_Button(root, 'Set Velocity', butt_set_v, 8, 1), \
+    Push_Button(root, 'Set Velocity', butt_set_vel, 8, 1), \
     Push_Button(root, 'QUIT', quit, 9, 1)]
 
 labels = [Text_Label(root, "The Odd Squad presents: 3 Body Orbital Mechanics", \
--- src/entry.py
+++ src/entry.py
@@ -1,11 +1,10 @@
 from tkinter import *
 
 class Text_Entry:
-    def __init__(self, root, row, column):
+    def __init__(self, root, value, row, column):
         self.entry = Entry(root)
         self.entry.grid(row=row, column=column)
+        self.entry.insert(0, value)
 
-    def get_entry(self):
+    def get_value(self):
         return self.entry.get()
-
-    
