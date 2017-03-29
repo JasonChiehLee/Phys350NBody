75c21b83bea41fa70708473145870fc63b2a5985
Jason Chieh Lee <jasonunivlee@hotmail.com>
2017-03-29 11:16:06 -0700

    oh baby, added entries/sliders for x and y, and u and v

--- src/GUI.py
+++ src/GUI.py
@@ -9,8 +9,8 @@ root = Tk()
 root.wm_title("PHYS 350: 3 Body Orbital Mechanics")
 
 mass_list = [0, 0, 0]
-vel_list = [0, 0, 0]
-xy = [0, 0]
+vel_list = [0, 0, 0, 0, 0, 0]
+xy = [0, 0, 0, 0, 0, 0]
 
 def butt_set_mass():
     for i in range(0,3):
@@ -18,12 +18,12 @@ def butt_set_mass():
         mass_sliders[i].set_value(mass_list[i])
 
 def butt_set_vel():
-    for i in range(0,3):
+    for i in range(0,6):
         vel_list[i] = vel_entries[i].get_value()
         vel_sliders[i].set_value(vel_list[i])
 
 def butt_set_xy():
-    for i in range(0,2):
+    for i in range(0,6):
         xy[i] = xy_entries[i].get_value()
         xy_sliders[i].set_value(xy[i])
 
@@ -33,12 +33,12 @@ def slider_set_mass(event):
         mass_entries[i].set_value(mass_list[i])
 
 def slider_set_vel(event):
-    for i in range(0,3):
+    for i in range(0,6):
         vel_list[i] = vel_sliders[i].get_value()
         vel_entries[i].set_value(vel_list[i])
 
 def slider_set_xy(event):
-    for i in range(0,2):
+    for i in range(0,6):
         xy[i] = xy_sliders[i].get_value()
         xy_entries[i].set_value(xy[i])
 
@@ -50,37 +50,59 @@ mass_entries = [Text_Entry(root, 0, 2, 0), \
     Text_Entry(root, 0, 2, 1), \
     Text_Entry(root, 0, 2, 2)]
 
-vel_entries = [Text_Entry(root, 0, 6, 0), Text_Entry(root, 0, 6, 1), \
-    Text_Entry(root, 0, 6, 2)]
+vel_entries = [Text_Entry(root, 0, 6, 0), Text_Entry(root, 0, 9, 0), \
+    Text_Entry(root, 0, 6, 1), \
+    Text_Entry(root, 0, 9, 1), \
+    Text_Entry(root, 0, 6, 2), \
+    Text_Entry(root, 0, 9, 2)
+    ]
 
-xy_entries = [Text_Entry(root, 0, 10, 0), Text_Entry(root, 0, 10, 1)]
+xy_entries = [Text_Entry(root, 0, 13, 0), Text_Entry(root, 0, 16, 0), \
+    Text_Entry(root, 0, 13, 1), Text_Entry(root, 0, 16, 1), \
+    Text_Entry(root, 0, 13, 2), Text_Entry(root, 0, 16, 2)]
 
 mass_sliders = [Slider(root, slider_set_mass, 0, 1000, 150, HORIZONTAL, 3, 0), \
      Slider(root, slider_set_mass, 0, 1000, 150, HORIZONTAL, 3, 1), \
      Slider(root, slider_set_mass, 0, 1000, 150, HORIZONTAL, 3, 2)]
 
 vel_sliders = [Slider(root, slider_set_vel, 0, 1000, 150, HORIZONTAL, 7, 0), \
-    Slider(root, slider_set_vel, 0, 1000, 150, HORIZONTAL, 7, 1),
-    Slider(root, slider_set_vel, 0, 1000, 150, HORIZONTAL, 7, 2)]
+    Slider(root, slider_set_vel, 0, 1000, 150, HORIZONTAL, 10, 0), \
+    Slider(root, slider_set_vel, 0, 1000, 150, HORIZONTAL, 7, 1), \
+    Slider(root, slider_set_vel, 0, 1000, 150, HORIZONTAL, 10, 1), \
+    Slider(root, slider_set_vel, 0, 1000, 150, HORIZONTAL, 7, 2), \
+    Slider(root, slider_set_vel, 0, 1000, 150, HORIZONTAL, 10, 2) ]
 
-xy_sliders = [Slider(root, slider_set_xy, 0, 1000, 150, HORIZONTAL, 11, 0), \
-    Slider(root, slider_set_xy, 0, 1000, 150, HORIZONTAL, 11, 1)]
+xy_sliders = [Slider(root, slider_set_xy, 0, 1000, 150, HORIZONTAL, 14, 0), \
+    Slider(root, slider_set_xy, 0, 1000, 150, HORIZONTAL, 17, 0), \
+    Slider(root, slider_set_xy, 0, 1000, 150, HORIZONTAL, 14, 1), \
+    Slider(root, slider_set_xy, 0, 1000, 150, HORIZONTAL, 17, 1), \
+    Slider(root, slider_set_xy, 0, 1000, 150, HORIZONTAL, 14, 2), \
+    Slider(root, slider_set_xy, 0, 1000, 150, HORIZONTAL, 17, 2 )
+    ]
 
 butts = [Push_Button(root, 'Set Mass', butt_set_mass, 4, 1), \
-    Push_Button(root, 'Set Velocity', butt_set_vel, 8, 1), \
-    Push_Button(root, 'Set Coordinates', butt_set_xy, 11, 2), \
-    Push_Button(root, 'Start', butt_start, 12, 1), \
-    Push_Button(root, 'QUIT', quit, 13, 1)]
+    Push_Button(root, 'Set Velocity', butt_set_vel, 11, 1), \
+    Push_Button(root, 'Set Coordinates', butt_set_xy, 18, 1), \
+    Push_Button(root, 'Start', butt_start, 19, 1), \
+    Push_Button(root, 'QUIT', quit, 20, 1)]
 
-labels = [Text_Label(root, "The Odd Squad presents: 3 Body Orbital Mechanics", \
+labels = [Text_Label(root, "The Odd Squad Presents: 3 Body Orbital Mechanics", \
     0, 0, 1, 2), \
     Text_Label(root, "Mass 1", 1, 0, 1, 1), \
     Text_Label(root, "Mass 2", 1, 1, 1, 1), \
     Text_Label(root, "Mass 3", 1, 2, 1, 1), \
-    Text_Label(root, "Velocity 1", 5, 0, 1, 1), \
-    Text_Label(root, "Velocity 2", 5, 1, 1, 1), \
-    Text_Label(root, "Velocity 3", 5, 2, 1, 1), \
-    Text_Label(root, "x", 9, 0, 1, 1), \
-    Text_Label(root, "y", 9, 1, 1, 1)]
+    Text_Label(root, "x-Velocity", 5, 0, 1, 1), \
+    Text_Label(root, "y-Velocity", 8, 0, 1, 1), \
+    Text_Label(root, "x-Velocity", 5, 1, 1, 1), \
+    Text_Label(root, "y-Velocity", 8, 1, 1, 1), \
+    Text_Label(root, "x-Velocity", 5, 2, 1, 1), \
+    Text_Label(root, "y-Velocity", 8, 2, 1, 1), \
+    Text_Label(root, "x1", 12, 0, 1, 1), \
+    Text_Label(root, "y1", 15, 0, 1, 1), \
+    Text_Label(root, "x2", 12, 1, 1, 1), \
+    Text_Label(root, "y2", 15, 1, 1, 1), \
+    Text_Label(root, "x3", 12, 2, 1, 1), \
+    Text_Label(root, "y3", 15, 2, 1, 1)
+    ]
 
 root.mainloop()
