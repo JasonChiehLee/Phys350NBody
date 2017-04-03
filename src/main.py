d4f16a1943ebed815ecad2c1f4567c728207576b
Jason Chieh Lee <jasonunivlee@hotmail.com>
2017-04-02 20:44:24 -0700

    Can start multiple sessions using GUI.

--- src/main.py
+++ src/main.py
@@ -5,37 +5,37 @@ from plot import Plot
 import physics as phys
 from GUI import *
 #from scipy.constants import astronomical_unit as AU
+while True:
+    root = Tk()
+    gui = GUI(root)
+    root.mainloop()
+    root.destroy()
+    PLOT = gui.butt_start()
 
-root = Tk()
-gui = GUI(root)
-root.mainloop()
+    OBJ_1 = Object(gui.mass_list[0] * phys.MASS_SCALING, phys.RADIUS, \
+                   phys.State(gui.xy[0], gui.xy[1], gui.vel_list[0], gui.vel_list[1], 1))
+    OBJ_2 = Object(gui.mass_list[1] * phys.MASS_SCALING, phys.RADIUS, \
+                   phys.State(gui.xy[2], gui.xy[3], gui.vel_list[2], gui.vel_list[3], 2))
+    OBJ_3 = Object(gui.mass_list[2] * phys.MASS_SCALING, phys.RADIUS, \
+                   phys.State(gui.xy[4], gui.xy[5], gui.vel_list[4], gui.vel_list[5], 3))
 
-PLOT = gui.butt_start()
+    COLOUR_1 = 'blue'
+    COLOUR_2 = 'red'
+    COLOUR_3 = 'orange'
 
-OBJ_1 = Object(gui.mass_list[0] * phys.MASS_SCALING, phys.RADIUS, \
-               phys.State(gui.xy[0], gui.xy[1], gui.vel_list[0], gui.vel_list[1], 1))
-OBJ_2 = Object(gui.mass_list[1] * phys.MASS_SCALING, phys.RADIUS, \
-               phys.State(gui.xy[2], gui.xy[3], gui.vel_list[2], gui.vel_list[3], 2))
-OBJ_3 = Object(gui.mass_list[2] * phys.MASS_SCALING, phys.RADIUS, \
-               phys.State(gui.xy[4], gui.xy[5], gui.vel_list[4], gui.vel_list[5], 3))
-
-COLOUR_1 = 'blue'
-COLOUR_2 = 'red'
-COLOUR_3 = 'orange'
-
-PLOT.place_obj(OBJ_1, COLOUR_1)
-PLOT.place_obj(OBJ_2, COLOUR_2)
-PLOT.place_obj(OBJ_3, COLOUR_3)
-
-dt = phys.D_T
-
-for i in range(0, 10 ** 6):
-    OBJ_1.iterate_state(dt)
-    OBJ_2.iterate_state(dt)
-    OBJ_3.iterate_state(dt)
-    PLOT.clear_objs()
     PLOT.place_obj(OBJ_1, COLOUR_1)
     PLOT.place_obj(OBJ_2, COLOUR_2)
     PLOT.place_obj(OBJ_3, COLOUR_3)
-    print(OBJ_1.state.__str__() + '\t' + phys.get_accel(OBJ_1.state).__str__())
-    PLOT.get_plot().pause(1.0/1000)
+
+    dt = phys.D_T
+    print ("%s \n %s \n %s" % (gui.xy, gui.mass_list, gui.vel_list))
+    for i in range(0, 100):
+        OBJ_1.iterate_state(dt)
+        OBJ_2.iterate_state(dt)
+        OBJ_3.iterate_state(dt)
+        PLOT.clear_objs()
+        PLOT.place_obj(OBJ_1, COLOUR_1)
+        PLOT.place_obj(OBJ_2, COLOUR_2)
+        PLOT.place_obj(OBJ_3, COLOUR_3)
+        #print(OBJ_1.state.__str__() + '\t' + phys.get_accel(OBJ_1.state).__str__())
+        PLOT.get_plot().pause(1.0/1000)
