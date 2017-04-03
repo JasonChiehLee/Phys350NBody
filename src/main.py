51b48402060a5bc39eb4c970bd21b306d5f694b7
David Ma <David Ma>
2017-04-02 20:54:19 -0700

    CLeaned up orbital objects into lists

--- src/main.py
+++ src/main.py
@@ -12,30 +12,46 @@ root.mainloop()
 
 PLOT = gui.butt_start()
 
-OBJ_1 = Object(gui.mass_list[0] * phys.MASS_SCALING, phys.RADIUS, \
-               phys.State(gui.xy[0], gui.xy[1], gui.vel_list[0], gui.vel_list[1], 1))
-OBJ_2 = Object(gui.mass_list[1] * phys.MASS_SCALING, phys.RADIUS, \
-               phys.State(gui.xy[2], gui.xy[3], gui.vel_list[2], gui.vel_list[3], 2))
-OBJ_3 = Object(gui.mass_list[2] * phys.MASS_SCALING, phys.RADIUS, \
-               phys.State(gui.xy[4], gui.xy[5], gui.vel_list[4], gui.vel_list[5], 3))
+objectList = []
 
-COLOUR_1 = 'blue'
-COLOUR_2 = 'red'
-COLOUR_3 = 'orange'
+for i in range(0,3):
+    Obj = Object(gui.mass_list[i] * phys.MASS_SCALING, phys.RADIUS, \
+                phys.State(gui.xy[2 * i], gui.xy[2 * i + 1], gui.vel_list[i], gui.vel_list[i+1], i+1))
+    objectList.append(Obj)
 
-PLOT.place_obj(OBJ_1, COLOUR_1)
-PLOT.place_obj(OBJ_2, COLOUR_2)
-PLOT.place_obj(OBJ_3, COLOUR_3)
+#OBJ_1 = Object(gui.mass_list[0] * phys.MASS_SCALING, phys.RADIUS, \
+#               phys.State(gui.xy[0], gui.xy[1], gui.vel_list[0], gui.vel_list[1], 1))
+#OBJ_2 = Object(gui.mass_list[1] * phys.MASS_SCALING, phys.RADIUS, \
+#               phys.State(gui.xy[2], gui.xy[3], gui.vel_list[2], gui.vel_list[3], 2))
+#OBJ_3 = Object(gui.mass_list[2] * phys.MASS_SCALING, phys.RADIUS, \
+#               phys.State(gui.xy[4], gui.xy[5], gui.vel_list[4], gui.vel_list[5], 3))
+
+#COLOUR_1 = 'blue'
+#COLOUR_2 = 'red'
+#COLOUR_3 = 'orange'
+
+colourList = ['blue', 'red', 'orange']
+
+for i in range(0,len(objectList)):
+    PLOT.place_obj(objectList[i], colourList[i])
+
+#PLOT.place_obj(OBJ_1, COLOUR_1)
+#PLOT.place_obj(OBJ_2, COLOUR_2)
+#PLOT.place_obj(OBJ_3, COLOUR_3)
 
 dt = phys.D_T
 
 for i in range(0, 10 ** 6):
-    OBJ_1.iterate_state(dt)
-    OBJ_2.iterate_state(dt)
-    OBJ_3.iterate_state(dt)
     PLOT.clear_objs()
-    PLOT.place_obj(OBJ_1, COLOUR_1)
-    PLOT.place_obj(OBJ_2, COLOUR_2)
-    PLOT.place_obj(OBJ_3, COLOUR_3)
-    print(OBJ_1.state.__str__() + '\t' + phys.get_accel(OBJ_1.state).__str__())
+    for j in range(0,len(objectList)):
+        objectList[j].iterate_state(dt)
+        PLOT.place_obj(objectList[j], colourList[j])
+    #OBJ_1.iterate_state(dt)
+    #OBJ_2.iterate_state(dt)
+    #OBJ_3.iterate_state(dt)
+
+    #PLOT.place_obj(OBJ_1, COLOUR_1)
+    #PLOT.place_obj(OBJ_2, COLOUR_2)
+    #PLOT.place_obj(OBJ_3, COLOUR_3)
+    print(objectList[0].state.__str__() + '\t' + phys.get_accel(objectList[0].state).__str__())
     PLOT.get_plot().pause(1.0/1000)
