938b05131eb74b986cc47f7ca2f452d7aa6e8893
Winnie Mui <Winnie Mui>
2017-04-03 09:20:12 -0700

    Still doesn't work

--- src/error.py
+++ src/error.py
@@ -15,7 +15,7 @@ num_pts = 5
 dt_start = 10e-6
 err_pts = 20
 dt = []
-iter_list = np.zeros((1, num_pts))
+iter_list = np.zeros((num_pts, num_pts*err_pts), float)
 
 for i in range(0,3):
     Obj = Object(mass_list[i] * phys.MASS_SCALING, phys.RADIUS, \
@@ -25,13 +25,10 @@ for i in range(0,3):
 for i in range(0, num_pts):
 	dt.append(dt_start * i)
 
-for i in range(0, num_pts):
-	iter_list[i] = np.zeros((1, i * err_pts),float)
-
 # Only saves the data for Object 1's x-velocity
 for i in range(0, num_pts):
 	for j in range(0,i*err_pts):
-		iter_list[i, 0, j]=objectList[1].state.get_pos[0,0]
+		iter_list[i, j]=objectList[1].state.get_pos()[0]
 		for k in range(0, len(objectList)):
 			objectList[k].iterate_state(dt[i])
 
@@ -40,6 +37,7 @@ fig = plt.figure()
 ax1 = fig.add_subplot(111)
 
 for i in range(0, num_pts):
-	ax1.plt(np.linspace(0 , dt_start*i*(num_pts-1), num_pts), iter_list[i, 0,:])
+	temp = iter_list[i, 0:i*err_pts]
+	ax1.plt.plot(np.linspace(0 , dt_start*i*(num_pts-1), num_pts), temp)
 
 plt.show()
