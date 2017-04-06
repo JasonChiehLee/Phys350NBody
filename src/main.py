bf6c8414a2ed15085aabab0b874402f7c99df72a
David Ma <David Ma>
2017-04-06 15:36:47 -0700

    Initial animation rafactoring changes

--- src/main.py
+++ src/main.py
@@ -4,38 +4,10 @@ from object import Object
 from plot import Plot
 import physics as phys
 from GUI import *
-from matplotlib import animation as animation
-import numpy as np
+
+
 #from scipy.constants import astronomical_unit as AU
 
-dt = phys.D_T
-
-def init():
-    """init animation"""
-    global traceLineList, particleList
-    
-    for l in traceLineList:
-        l.set_data([],[])
-
-    for p in particleList:
-        p.set_data([],[])
-
-    return tuple(traceLineList) + tuple(particleList)
-
-def animate(k):
-    """perform animation step"""
-    global dt, objectList, traceLineList, particleList
-
-    # step each object forward once
-    for j in range(0,len(objectList)):
-        objectList[j].iterate_state(dt)
-        oldLine = traceLineList[j]
-        traceLineList[j].set_data(np.append(oldLine.get_xdata(),objectList[j].get_state().get_pos()[0]), \
-                                  np.append(oldLine.get_ydata(),objectList[j].get_state().get_pos()[1]))
-        particleList[j].set_data([objectList[j].get_state().get_pos()[0]], \
-                                 [objectList[j].get_state().get_pos()[1]])
-
-    return tuple(traceLineList) + tuple(particleList)
 
 
 while True:
@@ -47,35 +19,13 @@ while True:
 
     """To check for collision, call gui.collision_status().  If = 1, then
     collision is selected"""
-    ax = PLOT.get_plot().gca()
-    traceLineList = []
-    particleList = []
     colourList = ['blue', 'red', 'green']
     lineConfigList = ['b-', 'r-', 'g-']
     objConfigList = ['bo', 'ro', 'go']
 
-    objectList = []
+    
     phys.G_OBJECTS.clear()
-    for i in range(0, 3):
-        Obj = Object(gui.mass_list[i] * phys.MASS_SCALING, phys.State(gui.xy[2 * i], \
-             gui.xy[2 * i + 1], gui.vel_list[2 * i], gui.vel_list[2 * i+1], i+1))
-        objectList.append(Obj)
-        traceLine, = ax.plot([], [], lineConfigList[i], lw=1)
-        traceLineList.append(traceLine)
-        particleDiameter = Obj.get_radius()*2*phys.PLOT_DPI*phys.PLOT_SIZE/phys.GRID_SIZE
-        particleLine, = ax.plot([], [], objConfigList[i], ms=particleDiameter)
-        print(Obj.get_radius())
-        print(particleDiameter)
-        particleList.append(particleLine)
-
-    from time import time
-    t0 = time()
-    animate(0)
-    t1 = time()
-    interval = 1000 * dt - (t1 - t0)
-    animFig = PLOT.get_plot().figure(num=1, figsize = (phys.PLOT_SIZE, phys.PLOT_SIZE))
-    animFig.set_size_inches(phys.PLOT_SIZE, phys.PLOT_SIZE, forward = TRUE)
-    ani = animation.FuncAnimation(animFig, animate, frames=10**6, \
-                              interval=interval, blit=True, init_func=init)
+    
+    PLOT.start_anim(colourList, lineConfigList, objConfigList, gui)
 
     #PLOT.show()
--- src/plot.py
+++ src/plot.py
@@ -2,6 +2,12 @@
 
 import matplotlib.pyplot as plt
 
+# Animation imports
+import numpy as np
+from matplotlib import animation as animation
+import physics as phys
+from object import Object
+
 class Plot:
     """ Main grid/plot area. """
     def __init__(self, x_min, x_max, y_min, y_max):
@@ -11,7 +17,41 @@ class Plot:
         plt.axis([x_min, x_max, y_min, y_max])
         plt.grid(b="on")
         self.patchList = list()
+        self.ax = plt.gca()
+        self.traceLineList = [] # List of pbject tracelines
+        self.particleList = []  # List of object locations
+        self.objectList = []    # List of particles added
+        self.dt = phys.D_T
 
+    def init(self):
+        """init animation"""
+    
+        for l in self.traceLineList:
+            l.set_data([],[])
+
+        for p in self.particleList:
+            p.set_data([],[])
+
+        return tuple(self.traceLineList) + tuple(self.particleList)
+
+    def animate(self, k):
+        """perform animation step"""
+
+        # step each object forward once
+        for j in range(0,len(self.objectList)):
+            self.objectList[j].iterate_state(self.dt)
+            oldLine = self.traceLineList[j]
+            self.traceLineList[j].set_data(np.append(oldLine.get_xdata(),self.objectList[j].get_state().get_pos()[0]), \
+                                  np.append(oldLine.get_ydata(),self.objectList[j].get_state().get_pos()[1]))
+            self.particleList[j].set_data([self.objectList[j].get_state().get_pos()[0]], \
+                                 [self.objectList[j].get_state().get_pos()[1]])
+
+            if j == 1:
+                print("test")
+
+        return tuple(self.traceLineList) + tuple(self.particleList)
+
+    
     def place_obj(self, obj, colour):
         """ Add an object to the plot. """
         draw_obj = plt.Circle(obj.state.get_pos(), obj.radius, \
@@ -38,5 +78,26 @@ class Plot:
         """TEST FUNCTION _ BREAKS ENCAPSULATION"""
         return plt
 
+    def start_anim(self, colourList, lineConfigList, objConfigList, gui):
+        
+        for i in range(0, 3):
+            Obj = Object(gui.mass_list[i] * phys.MASS_SCALING, phys.State(gui.xy[2 * i], \
+                gui.xy[2 * i + 1], gui.vel_list[2 * i], gui.vel_list[2 * i+1], i+1))
+            self.objectList.append(Obj)
+            traceLine, = self.ax.plot([], [], lineConfigList[i], lw=1)
+            self.traceLineList.append(traceLine)
+            particleDiameter = Obj.get_radius()*2*phys.PLOT_DPI*phys.PLOT_SIZE/phys.GRID_SIZE
+            particleLine, = self.ax.plot([], [], objConfigList[i], ms=particleDiameter)
+            self.particleList.append(particleLine)
+
+        from time import time
+        t0 = time()
+        self.animate(0)
+        t1 = time()
+        interval = 1000 * self.dt - (t1 - t0)
+        animFig = plt.figure(num=1, figsize = (phys.PLOT_SIZE, phys.PLOT_SIZE))
+        animFig.set_size_inches(phys.PLOT_SIZE, phys.PLOT_SIZE, forward = True)
+        ani = animation.FuncAnimation(animFig, self.animate, frames=10**6, \
+                              interval=interval, blit=True, init_func=self.init)
     #def get_fig(self):
     #    return self.plt.figure()
