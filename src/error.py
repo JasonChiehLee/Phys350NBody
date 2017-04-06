46b3804c939883d722de62dbdcf113a8a840bc0e
Winnie Mui <Winnie Mui>
2017-04-06 13:07:22 -0700

    pushing to not merge

--- src/error.py
+++ src/error.py
@@ -11,11 +11,11 @@ vel_list = [-2.0e1, 1.0e2, 6.0e1, -6.0e1, -1.0e2, 1.0e2]
 """[u1, v1, ...] """
 xy = [3e3, 0e3, 0e3, 0e3, 2e3, -4e3]
 """[x1, y1, ...] """
-num_pts = 4
-dt_start = 10e-5
-err_pts = 50
+num_pts = 3
+dt_start = 10e-4
+err_pts = 100
 dt = []
-iter_list = np.zeros((num_pts, err_pts*np.power(2,(num_pts-1))), float)
+iter_list = np.zeros((num_pts, err_pts*np.power(2,(num_pts-1))))
 
 for i in range(0,3):
     Obj = Object(mass_list[i] * phys.MASS_SCALING, \
@@ -30,6 +30,7 @@ for i in range(0, num_pts):
 for i in range(0, num_pts):
 	for j in range(0,err_pts*np.power(2,i)):
 		iter_list[i, j]=objectList[0].state.get_vel()[0]
+        print(iter_list[i,j])
 		for k in range(0, len(objectList)):
 			objectList[k].iterate_state(dt[i])
 
@@ -39,6 +40,6 @@ ax1 = fig.add_subplot(111)
 
 for i in range(0, num_pts):
 	temp = iter_list[i, 0:err_pts*np.power(2, i)]
-	ax1.plot(np.linspace(0 , (dt[i]*(err_pts*np.power(2,i)-1)), err_pts*np.power(2,i)), temp)
-
+	ax1.plot(np.linspace(0 , (dt[i]*(err_pts*np.power(2,i)-1)), err_pts*np.power(2,i)), temp, label ='%f' %(dt[i]))
+ax1.legend()
 plt.show()
