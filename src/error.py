615cc7add6f2aa8f88b0780571515e927c5e07a4
Winnie Mui <Winnie Mui>
2017-04-17 02:13:22 -0700

    Cleaned up error code. Can now modify error_analysis to generate graphs and compare methods

--- src/error.py
+++ src/error.py
@@ -5,98 +5,213 @@ import matplotlib.pyplot as plt
 import physics as phys
 import numpy as np
 
-#mass_list = [5, 5, 5]
-#vel_list = [-2.0e1, 1.0e2, 6.0e1, -6.0e1, -1.0e2, 1.0e2]
-"""[u1, v1, ...] """
-#xy = [3e3, 0e3, 0e3, 0e3, 2e3, -4e3]
-"""[x1, y1, ...] """
+class Error:
+    #IC: different states, 2 is recommended
+    #rt: 1 = realtime, 0 = faketime
+    #method: RK4 = 0, symplectic = 1, velocity verlet = 2   
+    #TODO: flag for iterate
+    def __init__ (self, IC, rt, dt_start, num_pts, err_pts, method, stepsize):
+        if IC == 0:
+            self.mass_list = [5, 5, 5]
+            self.vel_list = [-2.0e1, 1.0e2, 6.0e1, -6.0e1, -1.0e2, 1.0e2]
+            self.xy = [3e3, 0e3, 0e3, 0e3, 2e3, -4e3]
+        if IC == 1:
+            self.mass_list = [1, 20, 1]
+            self.vel_list = [0.0, 140.0, 0.0, 0.0, 0.0, -140]
+            self.xy = [3e3, 0e3, 0e3, 0e3, -3000.0, 0.0]
+        if IC == 2:
+            self.mass_list = [1, 1, 30]
+            self.vel_list = [0.0, -50.0, 0.0, 50.0, 0.0, 0.0]
+            self.xy = [-50000.0, 0.0, 50000, 0.0, 0.0, 0.0]
 
-mass_list = [1, 20, 1]
-vel_list =  [0.0, 140.0, 0.0, 0.0, 0.0, -140]
-xy =        [3e3, 0e3, 0e3, 0e3, -3000.0, 0.0]
+        self.rt = rt
+        self.dt_start = dt_start
+        self.dt_start1 = dt_start
+        self.err_pts = err_pts
+        self.num_pts = num_pts
+        self.method = method
+        self.stepsize = stepsize
+        self.dt = []
+        self.iter_list = np.zeros((1, 1))
+        self.methods_list = np.zeros((3, self.err_pts))
+        self.iter_flag = 0
+        self.meth_flag = 0
 
-#mass_list = [1, 1, 30]
-#vel_list = [0.0, -50.0, 0.0, 50.0, 0.0, 0.0]
-#xy = [-50000.0, 0.0, 50000, 0.0, 0.0, 0.0]
+    def iterate(self):
+        if self.rt == 1:
+            self.iter_list = np.zeros((self.num_pts, self.err_pts*np.power(2,(self.num_pts-1))))
+            for i in range(0, self.num_pts):
+                self.dt.append(self.dt_start)
+                self.dt_start = self.dt_start/2
 
-num_pts = 2
-dt_start = 1.0e-1
-err_pts = 1000
-dt = []
-real_time = 1
-method = 0      #RK4 = 0, symplectic = 1, velocity verlet = 2   
-stepsize = 2
-iter_list = np.zeros((num_pts, err_pts*np.power(2,(num_pts-1))))
+            for p in range(0, self.num_pts):
+                objectList = []
+                for i in range(0,3):
+                    Obj = Object(self.mass_list[i] * phys.MASS_SCALING, \
+                            phys.State(self.xy[2 * i], self.xy[2 * i + 1], self.vel_list[2 * i], self.vel_list[2 * i+1], i+1))
+                    objectList.append(Obj)
+                 # Only saves the data for Object 1's x-velocity
+                for j in range(0, self.err_pts * np.power(2, p)):
+                    self.iter_list[p, j]=objectList[0].state.get_vel()[0]
+                    for k in range(0, len(objectList)):
+                        objectList[k].state = phys.iterate(objectList[k].get_state(), self.dt[p], self.method)
+                phys.G_OBJECTS.clear()
 
-if real_time == 0:
-    iter_list = np.zeros((num_pts, err_pts))
+        else:
+            self.iter_list = np.zeros((self.num_pts, self.err_pts))
+            for i in range(0, self.num_pts):
+                dt.append(self.dt_start)
+                self.dt_start = self.dt_start/self.stepsize
 
-for i in range(0, num_pts):
-    dt.append(dt_start)
-    if real_time == 1:
-        dt_start = dt_start/2
-    else:
-        dt_start = dt_start/stepsize
+            for p in range(0, self.num_pts):
+                objectList = []
+                for i in range(0,3):
+                    Obj = Object(self.mass_list[i] * phys.MASS_SCALING, \
+                            phys.State(self.xy[2 * i], self.xy[2 * i + 1], self.vel_list[2 * i], self.vel_list[2 * i+1], i+1))
+                    objectList.append(Obj)
+                for j in range(0, self.err_pts):
+                    self.iter_list[p, j]=objectList[0].state.get_vel()[1]
+                    for k in range (0, len(objectList)):
+                        objectList[k].state = phys.iterate(objectList[k].get_state(), self.dt[p], self.method)
+                phys.G_OBJECTS.clear()
+        self.iter_flag = 1
 
+    def plot_method(self):
+        if self.iter_flag != 1:
+            self.iterate()
+        if self.rt == 1:
+            self.plot_method_rt()
+        else:
+            self.plot_method_ft()
 
-for p in range(0, num_pts):
-    objectList = []
-    for i in range(0,3):
-        Obj = Object(mass_list[i] * phys.MASS_SCALING, \
-                phys.State(xy[2 * i], xy[2 * i + 1], vel_list[2 * i], vel_list[2 * i+1], i+1))
-        objectList.append(Obj)
-    if real_time == 1:
-     # Only saves the data for Object 1's x-velocity
-        for j in range(0, err_pts * np.power(2, p)):
-            iter_list[p, j]=objectList[0].state.get_vel()[0]
-            for k in range(0, len(objectList)):
-                objectList[k].state = phys.iterate(objectList[k].get_state(), dt[p], method)
-               # objectList[k].iterate_state(1.0)
-    else:
-        for j in range(0, err_pts):
-            iter_list[p, j]=objectList[0].state.get_vel()[1]
-            for k in range (0, len(objectList)):
-                objectList[k].state = phys.iterate(objectList[k].get_state(), dt[p], method)
-                #objectList[k].iterate_state(dt[p])
-    
-    phys.G_OBJECTS.clear()
+    #Plots the method over various time steps (real time)
+    def plot_method_rt(self):
+        fig1 = plt.figure()
+        ax1 = fig1.add_subplot(111)
 
-#For now plots the data
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
+        for i in range(0, self.num_pts):
+            temp = self.iter_list[i, 0 : self.err_pts * np.power(2, i)]
+            ax1.plot(np.linspace(0 , (self.dt[i] * (self.err_pts * np.power(2, i) - 1)), self.err_pts * np.power(2, i)), temp, label ='%f' %(self.dt[i]*1e3))
 
-for i in range(0, num_pts):
-    if real_time == 1:
-        temp = iter_list[i, 0 : err_pts * np.power(2, i)]
-        ax1.plot(np.linspace(0 , (dt[i] * (err_pts * np.power(2, i) - 1)), err_pts * np.power(2, i)), temp, label ='%f' %(dt[i]))
-    else:
-        temp = iter_list[i, 0:err_pts]
-        ax1.plot(np.linspace(0, dt_start*(err_pts-1), err_pts), temp, label = '%f' %(dt[i] * 1e9))
+        # Plots the y velocity vs time step as it is
+        ax1.legend(title = "Timesteps in 1e-3")
+        ax1.set_title('Y-Velocity of Object 1 with Varying Time Step')
+        plt.ylabel('Y-Velocity of Object 1')
+        plt.xlabel('Time')
+        #plt.show()
 
-# Plots the y velocity vs time step as it is
-ax1.legend(title = "Timesteps in 1e-9")
-ax1.set_title('Y-Velocity of Object 1 with Varying Time Step')
-plt.ylabel('Y-Velocity of Object 1')
-plt.xlabel('Time')
-plt.show()
+    def plot_method_ft(self):
+        #For now plots the data
+        fig1 = plt.figure()
+        ax1 = fig1.add_subplot(111)
 
-# Play around with the error
-if real_time == 0:
-    copy = np.zeros((num_pts, err_pts))
-    for i in range(0, (num_pts-1)):
-        copy[i, :] = iter_list[i+1, :]
-    err = copy - iter_list
+        for i in range(0, self.num_pts):
+            temp = self.iter_list[i, 0:self.err_pts]
+            ax1.plot(np.linspace(0, self.dt_start1*(self.err_pts-1), self.err_pts), temp, label = '%f' %(self.dt[i] * 1e9))
 
-    fig2 = plt.figure()
-    ax2 = fig2.add_subplot(111)
+        # Plots the y velocity vs time step as it is
+        ax1.legend(title = "Timesteps in 1e-9")
+        ax1.set_title('Y-Velocity of Object 1 with Varying Time Step')
+        plt.ylabel('Y-Velocity of Object 1')
+        plt.xlabel('Time')
+        #plt.show()
 
-    for i in range(0, (num_pts-1)):
-        ax2.plot(np.linspace(0, dt_start*(err_pts-1), err_pts), np.absolute(err[i,:]), label = '%f' %(dt[i]*1e9))
+    def abs_error(self):
+        if self.iter_flag != 1:
+            self.iterate()
+        if self.rt == 1:
+            self.abs_error_rt()
+        else:
+            self.abs_error_ft()
 
-    ax2.legend(title = "Timestep in 1e-9")
-    ax2.set_title('Absolute error of the Y-Velocity')
-    plt.ylabel('Absolute Error')
-    plt.xlabel('Time')
-    plt.show()
+    def abs_error_ft(self):
+        copy = np.zeros((self.num_pts, self.err_pts))
+        for i in range(0, (self.num_pts-1)):
+            copy[i, :] = self.iter_list[i+1, :]
+        err = copy - self.iter_list
 
-#@TODO absolute error with real time
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(111)
+
+        for i in range(0, (self.num_pts-1)):
+            ax2.plot(np.linspace(0, self.dt_start1*(self.err_pts-1), self.err_pts), np.absolute(err[i,:]), label = '%f' %(self.dt[i]*1e3))
+
+        ax2.legend(title = "Timestep in 1e-3")
+        ax2.set_title('Absolute error of the Y-Velocity')
+        plt.ylabel('Absolute Error')
+        plt.xlabel('Time')
+        #plt.show()
+
+    def abs_error_rt(self):
+        copy1 = np.zeros((self.num_pts, self.err_pts))
+        copy2 = np.zeros((self.num_pts, self.err_pts))
+        for i in range(0, (self.num_pts)):
+            for j in range(0, (self.err_pts)):
+                copy1[i, j] = self.iter_list[i, j*(i+1)]
+        for i in range(0, (self.num_pts-1)):
+            copy2[i, :] = copy1[i+1, :]
+        err = copy1 - copy2
+
+        fig2 = plt.figure()
+        ax2 = fig2.add_subplot(111)
+
+        for i in range(0, (self.num_pts-1)):
+            ax2.plot(np.linspace(0, self.dt_start1*(self.err_pts-1), self.err_pts), np.absolute(err[i,:]), label = '%f' %(self.dt[i]*1e3))
+
+        ax2.legend(title = "Timestep in 1e-3")
+        ax2.set_title('Absolute error of the Y-Velocity')
+        plt.ylabel('Absolute Error')
+        plt.xlabel('Time')
+        #plt.show()
+
+    def iterate_methods(self):
+        for p in range(0, 3):
+            objectList = []
+            for i in range(0,3):
+                Obj = Object(self.mass_list[i] * phys.MASS_SCALING, \
+                        phys.State(self.xy[2 * i], self.xy[2 * i + 1], self.vel_list[2 * i], self.vel_list[2 * i+1], i+1))
+                objectList.append(Obj)
+            for j in range(0, self.err_pts):
+                self.methods_list[p, j]=objectList[0].state.get_vel()[1]
+                for k in range (0, len(objectList)):
+                    objectList[k].state = phys.iterate(objectList[k].get_state(), self.dt_start1, p)
+            phys.G_OBJECTS.clear()
+        self.meth_flag = 1
+
+    def plot_methods(self):
+        if self.meth_flag != 1:
+            self.iterate_methods()
+        #For now plots the data
+        fig3 = plt.figure()
+        ax3 = fig3.add_subplot(111)
+
+        for i in range(0, 3):
+            temp = self.methods_list[i, 0:self.err_pts]
+            ax3.plot(np.linspace(0, self.dt_start1*(self.err_pts-1), self.err_pts), temp, label = 'Method %f' %(i))
+
+        # Plots the y velocity vs time step as it is
+        ax3.legend(title = "Method")
+        ax3.set_title('Y-Velocity of Object 1 with Varying Methods')
+        plt.ylabel('Y-Velocity of Object 1')
+        plt.xlabel('Time')
+        #plt.show()
+
+    def abs_err_methods(self):
+        if self.meth_flag != 1:
+            self.iterate_methods()
+        copy = np.zeros((3, self.err_pts))
+        copy[2, :] = self.methods_list[0, :]
+        for i in range(0, 2):
+            copy[i, :] = self.methods_list[i+1, :]
+        err = copy - self.methods_list
+
+        fig4 = plt.figure()
+        ax4 = fig4.add_subplot(111)
+        legend = ["RK4 - Symplectic", "Symplectic - Velocity Verlet", "Verlocity Verlet - RK4"]
+        for i in range(0, 3):
+            ax4.plot(np.linspace(0, self.dt_start1*(self.err_pts-1), self.err_pts), np.absolute(err[i,:]), label = legend[i])
+
+        ax4.legend(title = "Methods")
+        ax4.set_title('Absolute difference between methods')
+        plt.ylabel('Difference')
+        plt.xlabel('Time')
---
+++ src/error_analysis.py
@@ -0,0 +1,26 @@
+from error import Error
+import matplotlib.pyplot as plt
+#Error: IC, rt, dt_start, num_pts, err_pts, method, stepsize
+#IC = 0, 1, 2; 1 is recommended
+#rt default = 1
+#dt_start = initial timestep
+#num_pts = number of dts to try, the next dt will be dt/2
+#err_pts = initial number of points to iterate, goes up by a factor of 2 with each extra num_pts
+#method = 0 - RK4, 1 - symplectic, 2 - velocity verlet
+#stepsize default = 2
+
+err1 = Error(1, 1, 1.0e-1, 3, 1000, 1, 2)
+err1.plot_methods()
+err1.abs_err_methods()
+plt.show()
+err1.plot_method()
+err1.abs_error()
+
+err2 = Error(1, 1, 1.0e-1, 3, 1000, 2, 2)
+err2.plot_method()
+err2.abs_error()
+
+err3 = Error(1, 1, 1.0e-1, 3, 1000, 0, 2)
+err3.plot_method()
+err3.abs_error()
+plt.show()  
